6778da22bbd89588715fa99bdfdc9daa716d4634
Prakash <ppalaniyappan@autoeveramerica.com>
2018-06-21 10:30:32 -0700

    my first commit

---
+++ sampletest1/src/main/java/com/kia/App.java
@@ -0,0 +1,17 @@
+package com.kia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int aaa()
+	{
+		return 3 + 5;
+	}
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World from Java" );
+//    }
+}
---
+++ sampletest1/src/main/java/com/kia/calculator.java
@@ -0,0 +1,31 @@
+package com.kia;
+
+public class calculator {
+	
+	public int add(int a, int b)
+	{
+		int addition = a + b;
+		return addition;
+	}
+	
+	public int sub(int a, int b)
+	{
+		int subtract = a - b;
+		return subtract;
+	}
+	
+	public int mul(int a, int b)
+	{
+		int multiply = a * b;
+		return multiply;
+	}
+	
+	public int div(int a, int b)
+	{
+		int divide = a / b;
+		return divide;
+	}
+	
+	
+
+}
